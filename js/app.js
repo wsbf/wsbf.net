e040bf16dbf54dec32acdb87b7c28663e36f782d
Ben Shealy <bentshermann@gmail.com>
2015-11-02 22:37:38 -0500

    Added index, initial views, and starter app.js

---
+++ js/app.js
@@ -0,0 +1,34 @@
+"use strict";
+var app = angular.module("app", ["ngRoute", "ui.bootstrap"]);
+
+app.config(["$routeProvider", function($routeProvider) {
+	$routeProvider
+		.when("/", { templateUrl: "views/slider_main.html" })
+		.when("/philosophy", { templateUrl: "views/philosophy.html" })
+		.when("/staff", { templateUrl: "views/staff.html" })
+		.when("/history", { templateUrl: "views/history.html" })
+		.when("/schedule", { templateUrl: "views/schedule.html" })
+		.when("/charts", { templateUrl: "views/charts.html" })
+		.when("/equipment", { templateUrl: "views/equipment.html" })
+		.when("/recording", { templateUrl: "views/recording.html" })
+		.when("/booking", { templateUrl: "views/booking.html" })
+		.when("/blog", { templateUrl: "views/blog.html" })
+		.when("/join", { templateUrl: "views/join.html" })
+		.when("/underwriting", { templateUrl: "views/underwriting.html" })
+		.when("/psa", { templateUrl: "views/psa.html" })
+		.when("/contact", { templateUrl: "views/contact.html" })
+		.otherwise("/");
+}]);
+
+app.controller("PlaylistCtrl", ["$scope", "$http", function($scope, $http) {
+	$scope.playlist = [];
+
+	var getPlaylist = function() {
+		$http.get("http://www.wsbf.net/api/playlist/current.php")
+			.then(function(playlist) {
+				$scope.playlist = playlist;
+			});
+	};
+
+	getPlaylist();
+}]);
