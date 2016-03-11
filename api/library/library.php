3d856d7e05caff4f4c85913f4ab41e6756cf7d2e
WSBF Computer Engineer <computer@wsbf.net>
2016-03-11 10:52:55 -0500

    Added paging and genre filtering to library API

--- api/library/library.php
+++ api/library/library.php
@@ -6,8 +6,6 @@
  *
  * Get albums in the music library, or update
  * the music library.
- *
- * TODO: add page offset
  */
 require_once("../auth.php");
 require_once("../connect.php");
@@ -15,12 +13,15 @@ require_once("../connect.php");
 /**
  * Get a section of the album library.
  *
- * @param mysqli      MySQL connection
- * @param rotationID  rotation ID
+ * @param mysqli           MySQL connection
+ * @param rotationID       rotation ID
+ * @param general_genreID  general genre ID
+ * @param page             page offset
  * @return array of albums
  */
-function get_library($mysqli, $rotationID)
+function get_library($mysqli, $rotationID, $general_genreID, $page)
 {
+	$page_size = 200;
 	$keys = array(
 		"al.albumID",
 		"al.album_code",
@@ -31,12 +32,17 @@ function get_library($mysqli, $rotationID)
 		"u.preferred_name AS reviewer"
 	);
 
-	$query = "SELECT " . implode(",", $keys) . " FROM `libalbum` AS al "
-			. "INNER JOIN `libartist` AS ar ON al.artistID=ar.artistID "
-			. "LEFT OUTER JOIN `libreview` AS r ON r.albumID=al.albumID "
-			. "LEFT OUTER JOIN `users` AS u ON r.username=u.username "
-			. "WHERE al.rotationID='$rotationID';";
-	$result = $mysqli->query($query);
+	$q = "SELECT " . implode(",", $keys) . " FROM `libalbum` AS al "
+		. "INNER JOIN `libartist` AS ar ON al.artistID=ar.artistID "
+		. "LEFT OUTER JOIN `libreview` AS r ON r.albumID=al.albumID "
+		. "LEFT OUTER JOIN `users` AS u ON r.username=u.username "
+		. "WHERE al.rotationID='$rotationID' "
+		. (isset($general_genreID)
+			? "AND al.general_genreID = '$general_genreID' "
+			: "")
+		. "ORDER BY al.albumID DESC "
+		. "LIMIT "  . ($page * $page_size) . ", $page_size;";
+	$result = $mysqli->query($q);
 
 	$albums = array();
 	while ( ($a = $result->fetch_assoc()) ) {
@@ -69,14 +75,18 @@ if ( $_SERVER["REQUEST_METHOD"] == "GET" ) {
 		exit("Current user is not allowed to view the music library.");
 	}
 
-	$rotationID = $_GET["rotation"];
+	$rotationID = $_GET["rotationID"];
+	$general_genreID = $_GET["general_genreID"];
+	$page = $_GET["page"];
 
-	if ( !is_numeric($rotationID) || $rotationID < 1 || 7 < $rotationID ) {
+	if ( !is_numeric($rotationID)
+	  || (isset($general_genreID) && !is_numeric($general_genreID))
+	  || !is_numeric($page) ) {
 		header("HTTP/1.1 404 Not Found");
-		exit("Rotation ID is empty or invalid.");
+		exit("Invalid input.");
 	}
 
-	$albums = get_library($mysqli, $rotationID);
+	$albums = get_library($mysqli, $rotationID, $general_genreID, $page);
 	$mysqli->close();
 
 	header("Content-Type: application/json");
