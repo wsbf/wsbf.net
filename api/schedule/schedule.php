c3ae2285417fb5ed893f4a473d5239bfce99291a
WSBF Computer Engineer <computer@wsbf.net>
2016-04-08 11:51:52 -0400

    Added initial remove schedule API

--- api/schedule/schedule.php
+++ api/schedule/schedule.php
@@ -3,6 +3,8 @@
 /**
  * @file schedule/schedule.php
  * @author Ben Shealy
+ *
+ * Get or remove the current show schedule.
  */
 require_once("../connect.php");
 
@@ -48,17 +50,49 @@ function get_schedule($mysqli, $day)
 	return $schedule;
 }
 
-$day = $_GET["day"];
-
-if ( !is_numeric($day) || $day < 0 || 6 < $day ) {
-	header("HTTP/1.1 404 Not Found");
-	exit("Day of week is empty or invalid.");
+/**
+ * Remove the entire show schedule.
+ *
+ * @param mysqli MySQL connection
+ */
+function remove_schedule($mysqli)
+{
+	$q = "UPDATE `schedule` SET "
+		. "active = 0 "
+		. "WHERE active = 1;";
+	$mysqli->query($q);
 }
 
-$mysqli = construct_connection();
-$schedule = get_schedule($mysqli, $day);
-$mysqli->close();
+if ( $_SERVER["REQUEST_METHOD"] == "GET" ) {
+	$day = $_GET["day"];
 
-header("Content-Type: application/json");
-exit(json_encode($schedule));
+	if ( !is_numeric($day) || $day < 0 || 6 < $day ) {
+		header("HTTP/1.1 404 Not Found");
+		exit("Day of week is empty or invalid.");
+	}
+
+	$mysqli = construct_connection();
+	$schedule = get_schedule($mysqli, $day);
+	$mysqli->close();
+
+	header("Content-Type: application/json");
+	exit(json_encode($schedule));
+}
+else if ( $_SERVER["REQUEST_METHOD"] == "DELETE" ) {
+	// TODO: move auth code into function to enable
+	// selective authentication for each HTTP verb
+	exit;
+
+	$mysqli = construct_connection();
+
+	if ( !check_senior_staff($mysqli) ) {
+		header("HTTP/1.1 401 Unauthorized");
+		exit;
+	}
+
+	remove_schedule($mysqli);
+	$mysqli->close();
+
+	exit;
+}
 ?>
