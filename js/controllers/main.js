48f5276b700ee09edac827b6b996a7e70db950b0
Ben Shealy <bentshermann@gmail.com>
2016-07-09 16:15:21 -0400

    Added alt attributes to img elements, removed tabindex attributes, set navbar to close on route change

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,10 +1,14 @@
 "use strict";
 
 var mainModule = angular.module("app.main", [
-    "ui.bootstrap"
+	"ui.bootstrap"
 ]);
 
 mainModule.controller("MainCtrl", ["$scope", "$uibModal", function($scope, $uibModal) {
+	$scope.$on("$routeChangeSuccess", function() {
+		$scope.navCollapsed = false;
+	});
+
 	$scope.openMediaCenter = function() {
 		$uibModal.open({
 			templateUrl: "views/mediacenter.html",
