71ebe7ea426329a87fd14d375798601ebc41e69f
Angelo Carrabba <acarrabba6@gmail.com>
2018-03-07 13:01:01 -0500

    Adding new Images!

--- client/js/controllers/staff.js
+++ client/js/controllers/staff.js
@@ -6,37 +6,37 @@ staffModule.constant("staff", [
     {
 	position: "Chief Announcer",
 	email: "announcer@wsbf.net",
-	name: "Sean Brewer"
+	name: "Sean Brewer",
 	imageUrl: "images/staff/Sean_Brewer.jpg"
     },
     {
 	position: "Chief Engineer",
 	email: "chief@wsbf.net",
-	name: "Christian Steinmetz"
+	name: "Christian Steinmetz",
 	imageUrl: "images/staff/Christian_Steinmetz.jpg"
     },
     {
 	position: "Computer Engineer",
 	email: "computer@wsbf.net",
-	name: "Angelo Carrabba"
+	name: "Angelo Carrabba",
 	imageUrl: "images/staff/Angelo_Carrabba.jpg"
     },
     {
 	position: "Events Coordinator",
 	email: "events@wsbf.net",
-	name: "Jacob Lairson"
+	name: "Jacob Lairson",
 	imageUrl: "images/staff/Jacob_Lairson.jpg"
     },
     {
 	position: "General Manager",
 	email: "gm@wsbf.net",
-	name: "Michael Murad"
+	name: "Michael Murad",
 	imageUrl: "images/staff/Michael_Murad.jpg"
     },
     {
 	position: "Member At Large",
 	email: "member@wsbf.net",
-	name: "Luke Waldrop"
+	name: "Luke Waldrop",
 	imageUrl: "images/staff/Luke_Waldrop.jpg"
     },
     {
@@ -48,13 +48,13 @@ staffModule.constant("staff", [
     {
 	position: "Production Director",
 	email: "production@wsbf.net",
-	name: "Brady Sklar"
+	name: "Brady Sklar",
 	imageUrl: "images/staff/Brady_Sklar.jpg"
     },
     {
 	position: "Promotion Director",
 	email: "promo@wsbf.net",
-	name: "Christian Fultz"
+	name: "Christian Fultz",
 	imageUrl: "images/staff/Christian_Fultz.jpg"
     },
     {
