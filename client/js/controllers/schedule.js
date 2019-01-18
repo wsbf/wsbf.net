7d8331805f79661f7fb8dde7288460a421d3bea3
haroldhyte <me@haroldhyte.com>
2019-01-18 15:11:25 -0500

    Display Bugfixes
    Fixed issue not being able to listen to stream from iPad sized devices
    Fixed minor homepage display errors
    Completed work on proper formatting of show schedule times

--- client/js/controllers/schedule.js
+++ client/js/controllers/schedule.js
@@ -8,6 +8,14 @@ var scheduleModule = angular.module("app.schedule", [
 scheduleModule.controller("ScheduleCtrl", ["$scope", "$stateParams", "db", function($scope, $stateParams, db) {
 	$scope.days = db.getDefs("days");
 	$scope.show_times = db.getDefs("show_times");
+
+	$scope.show_times.$promise.then(function(result) {
+			result.forEach(function(element) {
+					let converted = new Date("1 JAN 1970 " + element.show_time + " EST");
+					element.show_time = converted.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + " ET";
+			});
+	});
+
 	$scope.today = new Date();
 	$scope.dayID = $stateParams.dayID || $scope.today.getDay();
 	$scope.schedule = db.Schedule.get($scope.dayID);
