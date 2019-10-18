59ecbe2a02af8d0fd369b7bb61a7a19f342ab49a
Garrett Duncan <garrettdunc@gmail.com>
2019-10-18 16:26:08 -0400

    Update staff

--- client/js/controllers/staff.js
+++ client/js/controllers/staff.js
@@ -12,20 +12,26 @@ staffModule.constant("staff", [
     {
 			position: "Chief Announcer",
 			email: "announcer@wsbf.net",
-			name: "Jake Fladd",
-			imageUrl: "images/staff/1819/Jake.jpg"
+			name: "Reggie Nesbit",
+			imageUrl: "images/staff/chair.jpg"
     },
     {
 			position: "Chief Engineer",
 			email: "chief@wsbf.net",
-			name: "Christian Steinmetz",
-			imageUrl: "images/staff/1819/Christian.jpg"
+			name: "McCormick Emge",
+			imageUrl: "images/staff/chair.jpg"
     },
     {
 			position: "Computer Engineer",
 			email: "computer@wsbf.net",
-			name: "Harold Hyte",
-			imageUrl: "images/staff/1819/Harold.jpg"
+			name: "Garrett Duncan",
+			imageUrl: "images/staff/chair.jpg"
+    },
+    {
+			position: "Computer Engineer",
+			email: "computer@wsbf.net",
+			name: "Joe Dunn",
+			imageUrl: "images/staff/chair.jpg"
     },
     {
 			position: "Events Coordinator",
@@ -36,33 +42,45 @@ staffModule.constant("staff", [
     {
 			position: "Music Director",
 			email: "music@wsbf.net",
-			name: "Lizzy Parnell",
-			imageUrl: "images/staff/1819/Lizzy.jpg"
+			name: "Kylie Miller",
+			imageUrl: "images/staff/chair.jpg"
     },
     {
 			position: "Production Director",
 			email: "production@wsbf.net",
-			name: "Preston Dunnavant",
-			imageUrl: "images/staff/1819/Preston.jpg"
+			name: "Lizzy Parnell",
+			imageUrl: "images/staff/1819/Lizzy.jpg"
     },
     {
 			position: "Promotion Director",
 			email: "promo@wsbf.net",
 			name: "Sam Taylor",
 			imageUrl: "images/staff/chair.jpg"
-		},
-		{
+	},
+	{
 			position: "Member At Large",
 			email: "member@wsbf.net",
-			name: "Marissa Splendore",
+			name: "Tim Endredi",
 			imageUrl: "images/staff/chair.jpg"
-		}/*,
+	},
+	{
+			position: "Member At Large",
+			email: "member@wsbf.net",
+			name: "Kaitlyn Root",
+			imageUrl: "images/staff/chair.jpg"
+	},
+	{
+			position: "Member At Large",
+			email: "member@wsbf.net",
+			name: "Dan Watson",
+			imageUrl: "images/staff/chair.jpg"
+	}/*,
     {
 			position: "Student Media Advisor",
 			email: "jalexa5@g.clemson.edu",
 			name: "Jackie Alexander",
 			imageUrl: "images/wsbflady_100.png"
-		}*/
+	}*/
 ]);
 
 staffModule.controller("StaffCtrl", ["$scope", "staff", function($scope, staff) {
