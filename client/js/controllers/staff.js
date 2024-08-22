e1ad3fe67eed8f39b581ef4fae5c1bb60e66439c
Jai Agarwal <jaiveer.agarwal@gmail.com>
2024-08-21 20:46:01 -0400

    Adding controller for staff pics

--- client/js/controllers/staff.js
+++ client/js/controllers/staff.js
@@ -6,20 +6,20 @@ staffModule.constant("staff", [
 		{
 			position: "General Manager",
 			email: "gm@wsbf.net",
-			name: "Manning Holmes",
-			imageUrl: "images/staff/chair.jpg"
+			name: " Thomas Williams",
+			imageUrl: "images/staff/2425/thomas-williams.png"
 		},
     {
 			position: "Chief Announcer",
 			email: "announcer@wsbf.net",
-			name: "Dev Bryant",
-			imageUrl: "images/staff/chair.jpg"
+			name: "Megan Nash",
+			imageUrl: "images/staff/2425/megan-nash.png"
     },
     {
 			position: "Chief Engineer",
 			email: "chief@wsbf.net",
-			name: "Timothy Ross",
-			imageUrl: "images/staff/chair.jpg"
+			name: "Bernardo Vargas",
+			imageUrl: "images/staff/2425/bernardo-vargas.png"
     },
     /*{
 			position: "Equipment Engineer",
@@ -30,46 +30,40 @@ staffModule.constant("staff", [
     {
 			position: "Computer Engineer",
 			email: "computer@wsbf.net",
-			name: "Alexander Shelton",
-			imageUrl: "images/staff/chair.jpg"
+			name: "Jai Agarwal",
+			imageUrl: "images/staff/2425/jai-agarwal.png"
     },
     {
 			position: "Events Coordinator",
 			email: "events@wsbf.net",
-			name: "Reid Stapleton",
-			imageUrl: "images/staff/chair.jpg"
+			name: "Jack Waldrop",
+			imageUrl: "images/staff/2425/jack-waldrop.png"
     },
     {
 			position: "Music Director",
 			email: "music@wsbf.net",
-			name: "Thomas Williams",
-			imageUrl: "images/staff/chair.jpg"
+			name: "Nick Brenkert",
+			imageUrl: "images/staff/2425/nick-brenkert.png"
     },
     {
 			position: "Production Director",
 			email: "production@wsbf.net",
-			name: "Emery Cohen",
-			imageUrl: "images/staff/chair.jpg"
-    }
-    /*{
+			name: "Sean Adams",
+			imageUrl: "images/staff/2425/sean-adams.png"
+    },
+    {
 			position: "Promotion Director",
 			email: "promo@wsbf.net",
-			name: "Amanda Younglund",
-			imageUrl: "images/staff/chair.jpg"
+			name: "MG Corbin",
+			imageUrl: "images/staff/2425/mg-corbin.png"
 	},
 	{
 			position: "Member At Large",
 			email: "member@wsbf.net",
-			name: "Mick Thompson",
-			imageUrl: "images/staff/chair.jpg"
+			name: "Cole Turbeville",
+			imageUrl: "images/staff/2425/cole-turbeville.png"
 	},
-	{
-			position: "Member At Large",
-			email: "member@wsbf.net",
-			name: "Lizzy Parnell",
-			imageUrl: "images/staff/1819/Lizzy.jpg"
-	},
-    {
+    /*{
 			position: "Student Media Advisor",
 			email: "jalexa5@g.clemson.edu",
 			name: "Jackie Alexander",
