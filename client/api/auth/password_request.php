77734707e5981474c156899d066811c0b3ac5842
jai <124819199+jagarwa1@users.noreply.github.com>
2024-08-28 16:42:03 -0400

    Update password_request.php server and port

--- client/api/auth/password_request.php
+++ client/api/auth/password_request.php
@@ -4,6 +4,8 @@ error_reporting(-1);
 ini_set('display_errors', 'On');
 set_error_handler("var_dump");
 
+ini_set('SMTP','mail.privateemail.com' );
+ini_set('smtp_port', 465);
 /**
  * @file auth/password_request.php
  * @author Ben Shealy
