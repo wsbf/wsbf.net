6220559f7d0b54c6f593c9a17881c725423e07fc
Jai Agarwal <jaiveer.agarwal@gmail.com>
2024-08-29 17:54:25 -0400

    Updates to password reset email to use PHPMailer

--- client/api/auth/password_request.php
+++ client/api/auth/password_request.php
@@ -1,80 +1,113 @@
 <?php
 
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-
-ini_set('SMTP','mail.privateemail.com' );
-ini_set('smtp_port', 465);
 /**
  * @file auth/password_request.php
- * @author Ben Shealy
+ * @author ben shealy
+ * edits by jai agarwal
  */
 require_once("../connect.php");
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '/usr/share/php/PHPMailer/src/Exception.php';
+require '/usr/share/php/PHPMailer/src/PHPMailer.php';
+require '/usr/share/php/PHPMailer/src/SMTP.php';
 
 /**
  * Send an email.
- *
- * TODO: Although this function seems to be correct, the
- * mail that it sends is likely rejected by most
- * mail servers. More research should be done on
- * sendmail to make this function work.
- *
+ *	
+ * Using PHPMailer to send an email with SMTP
+ * through our new mail hosting service (Private Email)
+ * 
  * @param to       recipient email address
  * @param subject  subject
  * @param message  message
- * @return true if mail was accepted, false otherwise
+ * @return true if mail was sent, false otherwise
  */
 function send_mail($to, $subject, $message)
 {
-	$headers = "From: WSBF Computer Engineer <computer@wsbf.net>\r\n"
-		. "Content-Type: text/plain; charset=utf-8\r\n";
+//    $headers = "From: WSBF Computer Engineer <computer@wsbf.net>\r\n"
+//            . "Content-Type: text/plain; charset=utf-8\r\n";
 
-//	$headers = "MIME-Version: 1.0\r\n"
-//		. "Subject: $subject\r\n"
-//		. "X-Mailer: PHP/" . phpversion() . "\r\n";
+//    $headers = "MIME-Version: 1.0\r\n"
+//            . "Subject: $subject\r\n"
+//            . "X-Mailer: PHP/" . phpversion() . "\r\n";
 
-	return mail($to, $subject, $message, $headers);
+	//Create an instance; passing `true` enables exceptions
+	$mail = new PHPMailer(true);
+
+	try {
+		//Server settings
+		$mail->SMTPDebug  = SMTP::DEBUG_SERVER;                   //Enable verbose debug output
+		$mail->isSMTP();                                          //Send using SMTP
+		$mail->Host       = 'mail.privateemail.com';              //Set the SMTP server to send through
+		$mail->SMTPAuth   = true;                                 //Enable SMTP authentication
+		$mail->Username   = 'computer@wsbf.net';                  //SMTP username
+		$mail->Password   = 'longsufferingcompe';                 //SMTP password
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          //Enable implicit TLS encryption
+		$mail->Port       = 465;                                  // 465 is TCP port to connect to; use 587 if using `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+		//Recipients
+		$mail->setFrom('computer@wsbf.net', 'Mailer');
+		$mail->addAddress($to);     //Add a recipient
+		
+		
+		//Content
+		$mail->isHTML(true);                                  //Set email format to HTML
+		$mail->Subject = $subject;
+		$mail->Body    = $message;
+		$mail->AltBody = $message;
+
+		$mail->send();
+		
+		return true;
+		
+    } catch (Exception $e) {
+	    //file_put_contents('php://stderr', print_r("Message could not be sent. Mailer Error: {$mail->ErrorInfo}", TRUE));
+	    return false;
+	}
 }
 
 if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
-	$mysqli = construct_connection();
+        $mysqli = construct_connection();
 
-	// validate username
-	$username = $mysqli->escape_string($_GET["username"]);
+        // validate username
+        $username = $mysqli->escape_string($_GET["username"]);
 
-	$q = "SELECT username, email_addr FROM `users` WHERE username='$username';";
-	$result = exec_query($mysqli, $q);
+        $q = "SELECT username, email_addr FROM `users` WHERE username='$username';";
+        $result = exec_query($mysqli, $q);
 
-	if ( $result->num_rows == 0 ) {
-		header("HTTP/1.1 404 Not Found");
-		exit("Invalid username '$username'.");
-	}
+        if ( $result->num_rows == 0 ) {
+                header("HTTP/1.1 404 Not Found");
+                exit("Invalid username '$username'.");
+        }
 
-	// insert password reset request
-	$transaction_id = bin2hex(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM));
+        // insert password reset request
+        $transaction_id = bin2hex(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM));
 
-	$q = "DELETE FROM `password_reset` WHERE username = '$username';";
-	exec_query($mysqli, $q);
+        $q = "DELETE FROM `password_reset` WHERE username = '$username';";
+        exec_query($mysqli, $q);
 
-	$q = "INSERT INTO `password_reset` SET "
-		. "transaction_id = '$transaction_id',"
-		. "username = '$username',"
-		. "expiration_date = ADDDATE(CURDATE(), 7);";
-	exec_query($mysqli, $q);
+        $q = "INSERT INTO `password_reset` SET "
+                . "transaction_id = '$transaction_id',"
+                . "username = '$username',"
+                . "expiration_date = ADDDATE(CURDATE(), 7);";
+        exec_query($mysqli, $q);
 
-	// send email to user
-	$user = $result->fetch_assoc();
+        // send email to user
+        $user = $result->fetch_assoc();
 
-	$success = send_mail(
-		$user["email_addr"],
-		"Password Reset Request",
-		"Go to this link to reset your password: https://wsbf.net/login/#!/reset-password/$transaction_id"
-	);
+        $success = send_mail(
+                $user["email_addr"],
+                "Password Reset Request",
+                "Go to this link to reset your password: https://wsbf.net/login/#!/reset-password/$transaction_id"
+        );
 
-	$mysqli->close();
+        $mysqli->close();
 
-	header("Content-Type: application/json");
-	exit(json_encode($success));
+        header("Content-Type: application/json");
+        exit(json_encode($success));
 }
 ?>
