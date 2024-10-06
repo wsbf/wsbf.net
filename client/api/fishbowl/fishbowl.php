e516f404d329a00fa002c7429100aee2738d6ab7
Jai Agarwal <jaiveer.agarwal@gmail.com>
2024-10-06 15:09:43 -0400

    inline update rankings

--- client/api/fishbowl/fishbowl.php
+++ client/api/fishbowl/fishbowl.php
@@ -27,7 +27,7 @@ function get_fishbowl($mysqli)
 
 	$q = "SELECT " . implode(",", $keys) . " FROM `fishbowl_leaderboard` AS f "
 		. "INNER JOIN `users` AS u ON u.username=f.username "
-		. "ORDER BY f.points DSC;";
+		. "ORDER BY f.points DESC;";
 	$result = exec_query($mysqli, $q);
 
 	// compute the number of album reviews
